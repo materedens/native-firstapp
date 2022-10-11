0c47b08a9abad8cfea5b8e2c99f082898efc835d
Denis Matere <denismatere@gmail.com>
2022-10-11 16:33:49 +0300

    updates

---
+++ assets/cars.js
@@ -0,0 +1,8 @@
+const cars = [
+    {
+        id: 1,
+        name: 'Luxury',
+        img: require('../assets/image1.jpg'),
+    },
+];
+export default cars;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,74 @@
-import React from 'react';
-import { Text, View, StatusBar, StyleSheet, ImageBackground } from 'react-native';
+import React, { useState } from 'react';
+import {
+    Text, View, StatusBar,
+    StyleSheet, SafeAreaView,
+    TextInput, TouchableOpacity, FlatList, Dimensions
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import cars from '../assets/cars';
+
+const width = Dimensions.get('screen').width / 2 - 30;
 
 const HomeScreen = ({ navigation }) => {
-    return (
-        <View style={styles.container}>
-            <ImageBackground source={require('../assets/image1.jpg')}
-                style={{ width: "100%", height: "100%" }}>
+    //Arraylist
+    const cars = ['LUXURY', 'SUV', 'TOYOTA', 'ISUZU'];
+    const [carIndex, setCarIndex] = React.useState(0)
 
-                <StatusBar backgroundColor='#63c5da' barStyle="light-content" />
-                <View style={styles.header}>
-                    <Text style={styles.text}>Home Screen</Text>
-                </View>
-                <View style={styles.footer} >
+    const CarList = () => {
+        return (
+            <View style={styles.carContainer}>
+                {cars.map((item, index) => (
+                    <TouchableOpacity key={index}
+                        activeOpacity={0.8}
+                        onPress={() => setCarIndex(index)}>
+                        <Text
+                            style={[styles.carText, carIndex == index && styles.carSelected]}>
+                            {item}
+                        </Text>
+                    </TouchableOpacity>
+                ))}
+            </View>
+        );
+    };
+    const Card = ({ car }) => {
+        <View style={styles.card}>
 
-                    <Text style={styles.text}>Footer</Text>
-                </View>
-            </ImageBackground>
         </View>
+    };
+    return (
+        <SafeAreaView style={{
+            flex: 1, paddingHorizontal: 20,
+            backgroundColor: '#fff'
+        }}>
+            <StatusBar backgroundColor='#63c5da' barStyle="light-content" />
+            <View style={styles.header}>
+                <View>
+                    <Text style={styles.text}>Welcome to</Text>
+                    <Text style={{ fontSize: 35, fontWeight: 'bold', color: '#63c5da' }}>Luxury Car Shop</Text>
+                </View>
+                <Icon name='shopping-cart' size={20} />
+            </View>
+            <View style={{ marginTop: 30, flexDirection: 'row' }}>
+                <View style={styles.searchContainer}>
+                    <Icon name='search' size={20} style={{ marginLeft: 20 }} />
+                    <TextInput placeholder='Search' style={styles.input} />
+                </View>
+                <View style={styles.sortBtn}>
+                    <Icon name='sort' size={30} style={{ color: '#fff' }} />
+                </View>
+            </View>
+            <CarList />
+            <FlatList numColumns={2}
+                columnWrapperStyle={{ justifyContent: 'space-between' }}
+                showsVerticalScrollIndicator={false}
+                data={cars}
+                contentContainerStyle={{
+                    marginTop: 10,
+                    paddingBottom: 50,
+                }}
+                renderItem={({ item }) => <Card car={item} />}
+            />
+        </SafeAreaView>
     );
 }
 
@@ -29,26 +81,65 @@ const styles = StyleSheet.create({
     },
     text: {
         fontWeight: 'bold',
-        fontSize: 42,
-        textAlign: 'center',
+        fontSize: 25,
         fontFamily: "Roboto",
         color: '#000000',
-        lineHeight: 84,
+        // lineHeight: 84,
     },
     header: {
+        marginTop: 30,
+        justifyContent: 'space-between',
+        flexDirection: 'row'
+    },
+
+    searchContainer: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '28%',
+        flexDirection: 'row',
+        height: 50,
+        borderRadius: 10,
+        backgroundColor: '#f2f2f2',
+        alignItems: 'center'
     },
-    footer: {
-        flex: 3,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#fff',
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        paddingHorizontal: 20
+    input: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        flex: 1,
+        color: '#000000',
     },
+    sortBtn: {
+        marginLeft: 10,
+        height: 50,
+        width: 50,
+        backgroundColor: '#63c5da',
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    carContainer: {
+        flexDirection: 'row',
+        marginTop: 30,
+        marginBottom: 20,
+        justifyContent: 'space-between'
+    },
+    carText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#808080'
+    },
+    carSelected: {
+        color: '#63c5da',
+        paddingBottom: 5,
+        borderBottomWidth: 2,
+        borderColor: '#63c5da'
+    },
+    card: {
+        height: 255,
+        width,
+        marginHorizontal: 2,
+        borderRadius: 10,
+        marginBottom: 20,
+        padding: 15,
+        backgroundColor: '#F1F1F1'
+    }
 
 });
